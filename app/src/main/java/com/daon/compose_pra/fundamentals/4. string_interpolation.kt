d757fd3971b2aee5dbed0328a918726eae1b979d
daon1213 <diaeiyo1213@naver.com>
2022-12-07 13:40:49 +0900

    💡 문자열 입출력

---
+++ app/src/main/java/com/daon/compose_pra/fundamentals/4. string_interpolation.kt
@@ -0,0 +1,10 @@
+package com.daon.compose_pra.fundamentals
+
+fun main(){
+    val myStr = "Hello GSM"
+    var myLength = myStr.length
+
+    // 길이 출력하는 법
+    /* 1. */ print("My Str length is ${myStr.length}")
+    /* 2. */ print("My Str length is $myLength")
+}
