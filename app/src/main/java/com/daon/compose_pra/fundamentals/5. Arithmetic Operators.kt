5ec59f61dabefcff627d4e405be0889c46e6453b
daon1213 <diaeiyo1213@naver.com>
2022-12-07 13:42:34 +0900

    💡 산술 연산자

---
+++ app/src/main/java/com/daon/compose_pra/fundamentals/5. Arithmetic Operators.kt
@@ -0,0 +1,11 @@
+package com.daon.compose_pra.fundamentals
+
+fun main(){
+    // Arithmetic operators (+, -, *, /, %)
+    var result = 5 + 3
+    var a = 5.0
+    var b = 3
+    var resultDouble : Double
+    resultDouble = a / b
+    print(resultDouble)
+}
