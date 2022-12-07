a6a271c17ca81121c58ee6bd484bb3c30886058d
daon1213 <diaeiyo1213@naver.com>
2022-12-07 13:43:15 +0900

    💡 비교 연산자

---
+++ app/src/main/java/com/daon/compose_pra/fundamentals/6. Comparison Operators.kt
@@ -0,0 +1,15 @@
+package com.daon.compose_pra.fundamentals
+
+fun main(){
+    // Comparison operators (==, !-, <, >, <=, >=)
+    val isEqual = 5==3
+    println("isEqual is " + isEqual)
+
+    val isNotEqual = 5!=5
+    println("isNotEqual is " + isNotEqual)
+
+    println("is-5lessEqual3 ${-5<3}")
+    println("is5LowerEqual3 ${5 <= 3}")
+    println("is5HigherEqual3 ${5 >= 5}")
+
+}
