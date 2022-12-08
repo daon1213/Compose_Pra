0434456d84f1acc74e114e5c4c376cc653ee8355
daon1213 <diaeiyo1213@naver.com>
2022-12-08 10:51:06 +0900

    💡 조건문 기초

---
+++ app/src/main/java/com/daon/compose_pra/fundamentals/9. If_2.kt
@@ -0,0 +1,15 @@
+package com.daon.compose_pra.fundamentals
+
+fun main(){
+    var name = "Daon"
+
+    if (name == "Daon"){
+        println("Welcome home Daon")
+    }else{
+        println("Who are you?")
+    }
+
+    val isRainy = true
+    if (isRainy)
+        println("It's rainy")
+}
