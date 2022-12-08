e70358bb73dbae264ab73b5a1bee4aec305aea8a
daon1213 <diaeiyo1213@naver.com>
2022-12-08 10:50:15 +0900

    💡 다양한 연산자들

---
+++ app/src/main/java/com/daon/compose_pra/fundamentals/7. Various Operators.kt
@@ -0,0 +1,16 @@
+package com.daon.compose_pra.fundamentals
+
+fun main(){
+    // Assignment operators (+=, -=, *=, /=, %=)
+    var myNum = 5
+    myNum += 3
+    println("myNum is $myNum")
+    myNum *= 4
+    println("myNum is $myNum")
+
+    // Increment & Decrement operators (++, --)
+    myNum++
+    println("myNum is $myNum")
+    myNum--
+    println("myNum is $myNum")
+}
