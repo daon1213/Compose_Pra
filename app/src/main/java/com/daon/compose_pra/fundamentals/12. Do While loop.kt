38cb13746198c229b46220c8399cd0bccd1b5749
daon1213 <diaeiyo1213@naver.com>
2022-12-09 09:01:05 +0900

    💡 반복문 Do While

---
+++ app/src/main/java/com/daon/compose_pra/fundamentals/12. Do While loop.kt
@@ -0,0 +1,11 @@
+package com.daon.compose_pra.fundamentals
+
+fun main(){
+    var x = 1
+
+    do {
+        print("$x")
+        x++
+    }while (x <= 10)
+    println("\n do While loop is done")
+}
