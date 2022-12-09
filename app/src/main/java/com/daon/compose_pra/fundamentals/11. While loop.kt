99d3425351a10837237b98d764bed566275f2ce6
daon1213 <diaeiyo1213@naver.com>
2022-12-09 09:00:43 +0900

    💡 반복문 While

---
+++ app/src/main/java/com/daon/compose_pra/fundamentals/11. While loop.kt
@@ -0,0 +1,11 @@
+package com.daon.compose_pra.fundamentals
+
+fun main(){
+    // While loop executes a block of code repeatedly as long as a given condition is true
+    var x = 100
+    while (x <= 0) {
+        println("$x")
+        x -= 2
+    }
+    println("\n while loop is done")
+}
