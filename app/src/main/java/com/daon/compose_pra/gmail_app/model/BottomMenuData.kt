d0358127a2245829200a1b7b0e706e5a922e2af7
daon1213 <diaeiyo1213@naver.com>
2023-01-01 14:48:15 +0900

    💡 Compose UI 연습 Gmaill App

---
+++ app/src/main/java/com/daon/compose_pra/gmail_app/model/BottomMenuData.kt
@@ -0,0 +1,14 @@
+package com.daon.compose_pra.gmail_app.model
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material.icons.outlined.VideoCall
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomMenuData(
+    val icon: ImageVector,
+    val title: String
+) {
+    object Mail: BottomMenuData(icon = Icons.Outlined.Mail, "Mail")
+    object Meet: BottomMenuData(icon = Icons.Outlined.VideoCall, "Meet")
+}
