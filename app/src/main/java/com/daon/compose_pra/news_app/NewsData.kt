6d34e5a5b309f77f6a25dbc763ed7f718b36520d
daon1213 <diaeiyo1213@naver.com>
2023-02-04 13:15:06 +0900

    🔧 NewsData

---
+++ app/src/main/java/com/daon/compose_pra/news_app/NewsData.kt
@@ -0,0 +1,12 @@
+package com.daon.compose_pra.news_app
+
+import com.daon.compose_pra.R
+
+data class NewsData(
+    val id: Int,
+    val image: Int = R.drawable.breaking_news,
+    val author: String,
+    val title: String,
+    val description: String,
+    val publishedAt: String
+)
