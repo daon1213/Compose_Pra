f70d5241832406477373c0829cea7f852e4a6127
daon1213 <diaeiyo1213@naver.com>
2023-02-27 14:14:30 +0900

    🔧 Mock Data 시간 관련 로직 추가

--- app/src/main/java/com/daon/compose_pra/news_app/MockData.kt
+++ app/src/main/java/com/daon/compose_pra/news_app/MockData.kt
@@ -1,6 +1,10 @@
 package com.daon.compose_pra.news_app
 
+import android.os.Build
+import android.util.Log
 import com.daon.compose_pra.R
+import android.icu.text.SimpleDateFormat
+import java.util.*
 
 object MockData {
     val topNewsList = listOf<NewsData>(
@@ -69,5 +73,54 @@ object MockData {
 
     fun getNews(newsId: Int?):NewsData{
         return topNewsList.first{it.id == newsId}
+    }
+    //Todo 8: create time ago method as a Date extension function
+    fun Date.getTimeAgo(): String {
+        val calendar = Calendar.getInstance()
+        calendar.time = this
+
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        val currentCalendar = Calendar.getInstance()
+
+        val currentYear = currentCalendar.get(Calendar.YEAR)
+        val currentMonth = currentCalendar.get(Calendar.MONTH)
+        val currentDay = currentCalendar.get(Calendar.DAY_OF_MONTH)
+        val currentHour = currentCalendar.get(Calendar.HOUR_OF_DAY)
+        val currentMinute = currentCalendar.get(Calendar.MINUTE)
+
+        return if (year < currentYear) {
+            val interval = currentYear - year
+            if (interval == 1) "$interval year ago" else "$interval years ago"
+        } else if (month < currentMonth) {
+            val interval = currentMonth - month
+            if (interval == 1) "$interval month ago" else "$interval months ago"
+        } else if (day < currentDay) {
+            val interval = currentDay - day
+            if (interval == 1) "$interval day ago" else "$interval days ago"
+        } else if (hour < currentHour) {
+            val interval = currentHour - hour
+            if (interval == 1) "$interval hour ago" else "$interval hours ago"
+        } else if (minute < currentMinute) {
+            val interval = currentMinute - minute
+            if (interval == 1) "$interval minute ago" else "$interval minutes ago"
+        } else {
+            "a moment ago"
+        }
+    }
+    //Todo 9: Create a method to convert String to date
+    fun stringToDate(publishedAt: String): Date {
+        val date =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssxx", Locale.ENGLISH).parse(publishedAt)
+            } else {
+                java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssxx", Locale.ENGLISH).parse(publishedAt)
+            }
+        Log.d("published","$date")
+        return date
     }
 }
